b74cd9a4200a0a05904d170b215f553a6745eac8
Prakhar Maheshwari <prakhar108786@gmail.com>
2024-01-17 18:40:25 +0530

    cart data saved in local storage, mobile view is working

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
-import { Button, Drawer } from 'antd';
-import "./App.css"
-import Membership from './Membership';
-import MemberButton from './MemberButton';
-import ProductCost from './ProductCost';
+import React, { useState } from "react";
+import { Button, Drawer } from "antd";
+import "./App.css";
+import Membership from "./Membership";
+import MemberButton from "./MemberButton";
+import ProductCost from "./ProductCost";
+import axios from "axios";
 
-
-
-
-function Cart(){
+function Cart() {
   const [open, setOpen] = useState(false);
 
   const showDrawer = () => {
     setOpen(true);
+    if(document.getElementById("item"))
+    document.getElementById("item").classList.remove("null_display");
+    axios.post("https://plixlifehapi.farziengineer.co/rest/create_checkout/", {
+      checkoutInput: {
+        lines: [
+          {
+            quantity: 1,
+            variantId: 2113,
+          },
+        ],
+        email: "dummy@dummy.com",
+        isRecalculate: true,
+        tags: [],
+      },
+    }).then(( res)=>{
+      console.log(res.data);
+      // localStorage.setItem("qty", 1);
+      // if(!localStorage.key("qty")) {
+      //   localStorage.setItem("qty", 1)
+      // }
+    });
   };
 
   const onClose = () => {
@@ -20,18 +39,22 @@ function Cart(){
   };
   return (
     <>
-      <Button className='btn ant-btn' type='primary' onClick={showDrawer}>
-      Add To Cart
+      <Button className="btn ant-btn" type="primary" onClick={showDrawer}>
+        Add To Cart
       </Button>
-      <Drawer width={500} className='fontsize'   title="Shopping Cart" onClose={onClose} open={open}>
-      <Membership/>
-      <MemberButton/>
-      <ProductCost/>
+      <Drawer
+        width={500}
+        className="fontsize"
+        title="Shopping Cart"
+        onClose={onClose}
+        open={open}
+      >
+        <Membership />
+        <MemberButton />
+        <ProductCost />
       </Drawer>
     </>
   );
-};
-
-
+}
 
 export default Cart;
--- src/components/ProductCost.js
+++ src/components/ProductCost.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css"
 function ProductCost(){
-    const[val,setVal] = useState(1);
     
-    const increase = () => {
-        setVal(val+1);
-    }
+    const [qty, setQty] = useState(0);
 
+    useEffect(() => {
+      const storedQty = localStorage.getItem('qty');
+      if (storedQty !== null) {
+        setQty(parseInt(storedQty, 10));
+      }
+    }, []); 
+  
+    const increase = () => {
+      document.getElementById('item').classList.remove('null_display');
+      localStorage.setItem('qty', qty + 1);
+      setQty(qty + 1);
+    };
+  
     const decrease = () => {
-        if(val<2) return;
-        else setVal(val-1);
-    }
+      if (qty === 1) {
+        localStorage.setItem('qty', 0);
+        document.getElementById('item').classList.add('null_display');
+      } else {
+        localStorage.setItem('qty', qty - 1);
+        setQty(qty - 1);
+      }
+    };    
 return(
-    <div className="prod-cart-details">
+    <div className="prod-cart-details" id="item">
         <div className="cart-data">Product Added :</div>
         <div className="cart-cost">
             <img className="pro-cart-img" src="https://images.plixlife.com/products/762-c81e175599414d2e8af824fb9490ba1c.jpg?auto=format&sharp=20&ixlib=react-9.3.0&w=80&h=80&dpr=1&q=75"
@@ -26,7 +41,7 @@ return(
         <div className="cart-counter-parent">
         <div className="cart-counter">
             <button className="cart-quan-dec" onClick={decrease}>-</button>
-            <span>{val}</span>
+            <span>{qty}</span>
             <button className="cart-quan-inc" onClick={increase}>+</button>
             </div>
             <div className="cart-last-detail">
@@ -36,7 +51,7 @@ return(
             </div>
         </div>
         <div className="proceed-to-pay">
-            <span className="final-amount">Total ₹{val*649}</span>
+            <span className="final-amount">Total ₹{qty*649}</span>
             <button className="checkout">Proceed To Pay</button>
         </div>
     </div>  
